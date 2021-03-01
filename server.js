563f91d23617cb82b4fa6b67cf78ac2da32c9876
Felix Pohlmann <felixpohlmann.dev@gmail.com>
2021-03-01 21:06:30 +0100

    basic socket.io setup

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
 const cors = require("cors");
 
 //routes
@@ -10,6 +12,30 @@ const PORT = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//socket (need http server to be created)
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on("connection", (socket) => {
+  console.log("new client connected");
+  /*
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => testEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
+   */
+});
+
+const testEmit = (socket) => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("FromAPI", response);
+};
 
 //routes
 
