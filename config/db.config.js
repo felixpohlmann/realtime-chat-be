c5384d22fcb4831d3006a6851889ee742f73e879
Felix Pohlmann <felixpohlmann.dev@gmail.com>
2021-03-06 19:17:28 +0100

    added config folder

---
+++ config/db.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  dbUri:
+    "postgres://wudtlkdiernkmt:8b916479bbcaf70faf51cbb7c745667094e1ba5e8545b78edafbcb30f039cfaf@ec2-54-197-228-62.compute-1.amazonaws.com:5432/deio20090vebek",
+};
